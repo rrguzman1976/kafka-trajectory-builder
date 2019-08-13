30926501b16ec33023a83abdfa0cd2f456f5aa8f
rrguzman1976 <shermanflan@gmail.com>
2019-08-13 02:38:09 -0500

    One more

--- trajectory-consumer/main.py
+++ trajectory-consumer/main.py
@@ -10,16 +10,17 @@ def main():
         'bootstrap.servers': 'localhost:9092',
         'group.id': 'ds-consumer-python-01',
         'auto.offset.reset': 'earliest',
-        #'enable.auto.commit': 'false'
+        'enable.auto.commit': 'true'
     })
     
     try:
         c.subscribe(['funky-chicken.dbo.DbzWell'])
-            
+        
+        iteration = 1
         while True:
-            print(f"Polling...")
+            print(f"Polling {iteration}...")
             msg = c.poll(2.0)
-            #time.sleep(2)
+            iteration += 1
             
             if not msg:
                 continue
@@ -29,8 +30,9 @@ def main():
 
             print(f"Received message key: {msg.key().decode('utf-8')}")
             print(f"Received message: {msg.value().decode('utf-8')}")
-            c.commit()
-
+            
+            #c.commit() # if auto-commit = false
+            
     except Exception as e:
         print(f"Unhandled error: {e}")
     finally:
