f3444f8de5f4223e8ceb1bcd35da1c55309def95
rrguzman1976 <shermanflan@gmail.com>
2019-08-18 12:44:15 -0500

    Error handling

--- rko-down-all.sh
+++ rko-down-all.sh
@@ -4,6 +4,6 @@
 export DEBEZIUM_VERSION=0.9
 
 docker-compose -f docker-compose-kafka.yml down
-docker-compose -f docker-compose-consumer.yml down
+docker-compose -f docker-compose-consumer.yml down || echo 'Docker consumer does not exist...'
 docker container prune -f
 docker image prune -f
